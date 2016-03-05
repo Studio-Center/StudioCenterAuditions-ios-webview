91bcc4bc27bf0ab646a09b3e6b3bb3279e5cf107
Robert McDowell <doodersrage@gmail.com>
2016-03-04 21:02:49 -0500

    Initial Commit

---
+++ Studio Center Auditions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Studio Center Auditions
+//
+//  Created by Robert McDowell on 3/4/16.
+//  Copyright © 2016 Robert McDowell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Studio Center Auditions/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Studio Center Auditions
+//
+//  Created by Robert McDowell on 3/4/16.
+//  Copyright © 2016 Robert McDowell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
