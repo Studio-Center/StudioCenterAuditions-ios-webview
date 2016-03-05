9eacf568f2f81a1ea389f92a1d48c4e78cdcdd63
Robert McDowell <doodersrage@gmail.com>
2016-03-05 13:55:28 -0500

    test launch view + resize web view full screen

--- Studio Center Auditions/ViewController.m
+++ Studio Center Auditions/ViewController.m
@@ -17,18 +17,22 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+        
     self.automaticallyAdjustsScrollViewInsets = NO;
     
     // Load the url into the webview
     NSURL *url = [NSURL URLWithString:@"http://studiocenterauditions.com"];
     [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
+
+}
+
+- (void)viewDidLayoutSubviews {
+    self.webView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 } // Dispose of any resources that can be recreated.
 
 
